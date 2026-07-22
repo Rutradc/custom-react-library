26b7fe59f9411533fbbf6b8cc3826668613e4a9e
CorentinLeonet <leonetcorentin@gmail.com>
2026-07-22 10:58:20 +0200

    chore(sidebar): add placeholder nav links for dev testing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import SideBar from './components/sidebar/SideBar.jsx'
+import { navLinks } from './config/navLinks.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SideBar links={navLinks} />
     <App />
   </StrictMode>,
 )
