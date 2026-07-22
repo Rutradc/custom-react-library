b5c6b629864076addf20a2364b4ffb733a1b6109
CorentinLeonet <leonetcorentin@gmail.com>
2026-07-22 15:11:41 +0200

    feat: Add checkbox with dynamic label

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { useState } from 'react'
 import './App.css'
+import { Checkbox } from './components'
 
 function App() {
-
+  const [agreed, setAgreed] = useState(false)
   return (
     <>
-      
+      <Checkbox checked={agreed} onChange={setAgreed} label="I agree to the terms" />
     </>
   )
 }
