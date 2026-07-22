5cd5dbf9cff11625afcda4859d88f2a17f7e7f68
CorentinLeonet <leonetcorentin@gmail.com>
2026-07-22 11:13:35 +0200

    fix(sidebar): use named import for barrel file export

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import SideBar from './components/sidebar/SideBar.jsx'
+import { SideBar } from './components'
 import { navLinks } from './config/navLinks.js'
 
 createRoot(document.getElementById('root')).render(
