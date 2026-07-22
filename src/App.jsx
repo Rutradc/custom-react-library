52c7436479c2aa6995436700a7db18e97c34299b
CorentinLeonet <leonetcorentin@gmail.com>
2026-07-22 12:24:10 +0200

    chore(select): add placeholder select for dev testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'
+import { Select } from './components'
+import { useState } from 'react'
+
 
 function App() {
 
+  const [selected, setSelected] = useState('')
+
+  
+
   return (
     <>
+      <Select
+          options={[
+              { value: 'be', label: 'Belgium' },
+              { value: 'fr', label: 'France' },
+          ]}
+          value={selected}
+          onChange={setSelected}
+          placeholder="-- Choose a country --"
+      />
       
     </>
   )
