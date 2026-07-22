a954ae2957b34d77a1e8097a299d9cfdf01f7b45
Jerome Verkyndt <j.verkyndt@students.ephec.be>
2026-07-22 15:52:12 +0200

    Add modal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Button } from './components';
+import { Modal } from './components';
 
 function App() {
+
   return (
     <div style={{ padding: '2rem', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
       <h2>Test de mes boutons</h2>
@@ -43,7 +45,74 @@ function App() {
           disabled={true} 
         />
       </div>
+
+
+      <h2>Test de ma modal</h2>
+
+
+      <Modal
+        title="Petite modal"
+        size="small"
+        trigger={<Button 
+        label="petite modal"
+        primary
+        />
+        }
+      >
+        <p>Contenu</p>
+      </Modal>
+      
+
+      <Modal
+        title="Moyenne modal"
+        size="medium"
+        trigger={<Button 
+        label="moyenne modal"
+        primary
+        />
+        }
+      >
+        <p>Contenu</p>
+      </Modal>
+      
+
+
+      <Modal
+        title="Grande modal"
+        size="large"
+        trigger={<Button 
+        label="grande modal"
+        primary
+        />
+        }
+      >
+        <p>Contenu</p>
+      </Modal>
+
+      <Modal
+        title="Supprimer un élément"
+        trigger={<Button 
+        label="Supprimer un élément"
+        primary
+        />
+        }
+        size="small"
+        showActions={true}
+        confirmText="Supprimer"
+        cancelText="Annuler"
+        onConfirm={() => {
+          console.log('Élément supprimé');
+        }}
+      >
+        <p>
+          Êtes-vous sûr de vouloir supprimer cet élément ?
+        </p>
+      </Modal>
+            
+
+
     </div>
+
   );
 }
 
