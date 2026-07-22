487e6224fe3fcff2fdf5244a9a4f8df424f043af
CorentinLeonet <leonetcorentin@gmail.com>
2026-07-22 11:21:19 +0200

    chore(sidebar): move sidebar from main.jsx to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { SideBar } from './components'
+import { navLinks } from './config/navLinks.js'
+
 
 function App() {
 
   return (
     <>
-      
+      <SideBar links={navLinks} />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { SideBar } from './components'
-import { navLinks } from './config/navLinks.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SideBar links={navLinks} />
     <App />
   </StrictMode>,
 )
